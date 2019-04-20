f5f9176369df7da596d8c9175af5c81375d55d2d
Otavio Salvador <otavio@ossystems.com.br>
2019-04-19 21:35:07 -0300

    linuxkernel: Refactor the code to allow addition of ARM images
    We need to detect the architecture of the binary so we can handle it
    accordingly; to do so, we need to abstract the detection from the
    version detection code.
    
    This commit prepares the basis for it, as it adds a new method to be
    used to detect the Linux kernel image type and consequently the
    architecture.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/linuxkernel.rs
+++ src/linuxkernel.rs
@@ -7,6 +7,47 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use regex::bytes::Regex;
 use std::io::{Read, Seek, SeekFrom};
 
+#[allow(clippy::enum_variant_names)]
+enum LinuxKernelKind {
+    X86bzImage,
+    X86zImage,
+}
+
+fn discover_linux_kernel_kind<R: Read + Seek>(buf: &mut R) -> Option<LinuxKernelKind> {
+    // Taken from: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/Documentation/x86/boot.txt#n144
+    //
+    // Offset  Proto   Name            Meaning
+    // /Size
+    // ...
+    // 01FE/2  ALL     boot_flag       0xAA55 magic number
+    // ...
+    // 0211/1	2.00+	loadflags	Boot protocol option flags
+
+    // Verify the boot_flag magic number
+    buf.seek(SeekFrom::Start(0x01FE)).ok()?;
+    if buf.read_u16::<LittleEndian>().ok()? != 0xAA55 {
+        return None;
+    }
+
+    // Field name:	loadflags
+    // Type:		modify (obligatory)
+    // Offset/size:	0x211/1
+    // Protocol:	2.00+
+    //
+    //   This field is a bitmask.
+    //
+    //   Bit 0 (read):	LOADED_HIGH
+    //         - If 0, the protected-mode code is loaded at 0x10000.
+    //         - If 1, the protected-mode code is loaded at 0x100000.
+    //   ...
+    buf.seek(SeekFrom::Start(0x0211)).ok()?;
+    match buf.read_u8().ok()? & 0x1 {
+        0 => Some(LinuxKernelKind::X86zImage),
+        1 => Some(LinuxKernelKind::X86bzImage),
+        _ => None,
+    }
+}
+
 pub(crate) struct LinuxKernel<'a, R>
 where
     R: Read + Seek,
@@ -25,54 +66,53 @@ impl<'a, R: Read + Seek> LinuxKernel<'a, R> {
 
 impl<'a, R: Read + Seek> VersionFinder for LinuxKernel<'a, R> {
     fn get_version(&mut self) -> Option<String> {
-        // Taken from: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/Documentation/x86/boot.txt#n144
-        //
-        // Offset  Proto   Name            Meaning
-        // /Size
-        // ...
-        // 01F1/1  ALL(1   setup_sects     The size of the setup in sectors
-        // ...
-        // 01FE/2  ALL     boot_flag       0xAA55 magic number
-        // ...
-        // 020E/2  2.00+   kernel_version  Pointer to kernel version string
+        let buffer = match discover_linux_kernel_kind(self.buf)? {
+            LinuxKernelKind::X86bzImage | LinuxKernelKind::X86zImage => {
+                // Taken from: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/Documentation/x86/boot.txt#n144
+                //
+                // Offset  Proto   Name            Meaning
+                // /Size
+                // ...
+                // 01F1/1  ALL(1   setup_sects     The size of the setup in sectors
+                // ...
+                // 020E/2  2.00+   kernel_version  Pointer to kernel version string
 
-        self.buf.seek(SeekFrom::Start(0x01F1)).ok()?;
-        let setup_sects = u64::from(self.buf.read_u8().ok()?);
+                // Get the setup_sects information
+                self.buf.seek(SeekFrom::Start(0x01F1)).ok()?;
+                let setup_sects = u64::from(self.buf.read_u8().ok()?);
 
-        // Verify the boot_flag magic number
-        self.buf.seek(SeekFrom::Start(0x01FE)).ok()?;
-        if self.buf.read_u16::<LittleEndian>().ok()? != 0xAA55 {
-            return None;
-        }
+                // Get kernel_version pointer
+                self.buf.seek(SeekFrom::Start(0x020E)).ok()?;
+                let kernel_version_ptr = u64::from(self.buf.read_u16::<LittleEndian>().ok()?);
 
-        // Get kernel_version pointer
-        self.buf.seek(SeekFrom::Start(0x020E)).ok()?;
-        let kernel_version_ptr = u64::from(self.buf.read_u16::<LittleEndian>().ok()?);
+                // Field name:     kernel_version
+                // Type:           read
+                // Offset/size:    0x20e/2
+                // Protocol:       2.00+
+                //
+                //   If set to a nonzero value, contains a pointer to a NUL-terminated
+                //   human-readable kernel version number string, less 0x200.  This can
+                //   be used to display the kernel version to the user.  This value
+                //   should be less than (0x200*setup_sects).
+                if kernel_version_ptr >= setup_sects * 0x200 {
+                    return None;
+                }
 
-        // Field name:     kernel_version
-        // Type:           read
-        // Offset/size:    0x20e/2
-        // Protocol:       2.00+
-        //
-        //   If set to a nonzero value, contains a pointer to a NUL-terminated
-        //   human-readable kernel version number string, less 0x200.  This can
-        //   be used to display the kernel version to the user.  This value
-        //   should be less than (0x200*setup_sects).
-        if kernel_version_ptr >= setup_sects * 0x200 {
-            return None;
-        }
+                // Move to the kernel version location
+                self.buf
+                    .seek(SeekFrom::Start(kernel_version_ptr + 0x200))
+                    .ok()?;
 
-        // Move to the kernel version location
-        self.buf
-            .seek(SeekFrom::Start(kernel_version_ptr + 0x200))
-            .ok()?;
+                // Read the Linux kernel version from the reader
+                let mut buffer = [0; 0x200];
+                self.buf.read(&mut buffer).ok()?;
 
-        // Read the Linux kernel version from the reader
-        let mut buffer = [0; 0x200];
-        self.buf.read(&mut buffer).ok()?;
+                buffer
+            }
+        };
 
         // Filter out unnecessary information
-        let re = Regex::new(r"(?P<version>\d+.?\.[^\s]+)").unwrap();
+        let re = Regex::new(r"(?P<version>\d+.?\.[^\s\u{0}]+)").unwrap();
         re.captures(&buffer)
             .and_then(|m| m.name("version"))
             .and_then(|v| std::str::from_utf8(v.as_bytes()).ok())
