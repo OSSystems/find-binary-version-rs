9310fe10a6fd1b162aa88033ef2706bc479cd5d7
Otavio Salvador <otavio@ossystems.com.br>
2021-02-12 08:20:12 -0300

    cargo: Bump compress-tools from 0.9 to 0.10
    This upgrades the crate and raise the MSRV to 1.44 as this is needed to
    use the new library.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@
 //! this. If building on *nix systems, `pkg-config` is used to locate the
 //! `libarchive`; on Windows `vcpkg` will be used to locating the `libarchive`.
 //!
-//! The minimum supported Rust version is 1.42.
+//! The minimum supported Rust version is 1.44.
 //!
 //! ## Features
 //!
