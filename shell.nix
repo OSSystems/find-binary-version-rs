a66565e8773fa1f5fc26346ad26a5cc97b5a4d89
Otavio Salvador <otavio@ossystems.com.br>
2021-02-12 08:41:25 -0300

    shell.nix: Remove compress-tools specific dependencies
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- shell.nix
+++ shell.nix
@@ -3,15 +3,9 @@
 with pkgs;
 
 stdenv.mkDerivation {
-  name = "compress-tools";
+  name = "find-binary-version";
   buildInputs = [
     pkg-config
     libarchive
-    clang
-    llvmPackages.libclang
   ];
-  # why do we need to set the library path manually?
-  shellHook = ''
-      export LIBCLANG_PATH="${llvmPackages.libclang}/lib";
-  '';
 }
