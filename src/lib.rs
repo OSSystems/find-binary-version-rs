3ef2bec2bbf1e945d05f443c1443b477abfaa22a
Otavio Salvador <otavio@ossystems.com.br>
2022-11-21 08:48:54 -0300

    lib: update MSRV reference
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@
 //! this. If building on *nix systems, `pkg-config` is used to locate the
 //! `libarchive`; on Windows `vcpkg` will be used to locating the `libarchive`.
 //!
-//! The minimum supported Rust version is 1.44.
+//! The minimum supported Rust version is 1.59.
 //!
 //! ## Features
 //!
