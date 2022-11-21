24b937a34ca2e7f3469ea078a9551d6104b5eaba
Otavio Salvador <otavio@ossystems.com.br>
2022-11-21 08:37:46 -0300

    flake: add MSRV Rust toolchain and pin it
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- flake.nix
+++ flake.nix
@@ -3,20 +3,44 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-21.11";
+
+    rust = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, rust, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        rust-toolchain = with rust.packages.${system};
+          let
+            channel_1_59_0 = {
+              channel = "1.59.0";
+              sha256 = "sha256-4IUZZWXHBBxcwRuQm9ekOwzc0oNqH/9NkI1ejW7KajU=";
+            };
+            stable_1_59_0 = toolchainOf channel_1_59_0;
+          in
+          combine [
+            (stable_1_59_0.withComponents [ "rustc" "cargo" "rust-src" "clippy" ])
+
+            latest.rustfmt
+            latest.rust-analyzer
+          ];
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
+            rust-toolchain
+            cargo-release
+
             pkg-config
             libarchive
           ];
